2a8fc2f619ceb06d05dffc1446571ef4b34bd2bf
Jonáš Rosecký <rosecky.jonas@centrum.cz>
2020-10-22 13:33:49 +0300

    API specification

--- res/js/common.js
+++ res/js/common.js
@@ -23,16 +23,51 @@ async function getUserInfo() {
     return await $.get(
         "https://private-anon-2b0cfa86a8-wad20postit.apiary-mock.com/users/1"
     );
+
+    /*
+        {
+            "firstname": string,
+            "lastname": string,
+            "email": string,
+            "avatar": string
+        }
+    */
 }
 
 async function getPosts() {
     return await $.get(
         "https://private-anon-2b0cfa86a8-wad20postit.apiary-mock.com/posts"
     );
+
+    /*
+        {
+            "id": number,
+            "author": {
+                "firstname": string,
+                "lastname": string,
+                "avatar": string
+            },
+            "createTime": string,
+            "text": null | string,
+            "media": null | {
+                "type": "image" | "video",
+                "url": string
+            },
+            "likes": string
+        }[]
+    */
 }
 
 async function getProfiles() {
     return await $.get(
         "https://private-anon-2b0cfa86a8-wad20postit.apiary-mock.com/profiles"
     );
+
+    /*
+        {
+            "firstname": string,
+            "lastname": string,
+            "avatar": string
+        } []
+    */
 }
