57ce796aa447bd2b91ff3f109beb7d8c648a2175
samar23abed <samar23abed@gmail.com>
2020-10-22 13:14:04 +0300

    add first subtask of task 2

--- res/js/script.js
+++ res/js/script.js
@@ -1,5 +1,6 @@
 $(function () {
     updateUserInfo();
+    updatPostsInfo();
 });
 
 function updateUserInfo() {
@@ -8,6 +9,44 @@ function updateUserInfo() {
         $(".avatar-container .avatar").attr("src", data.avatar);
 
         // TODO: update also the menu
+    });
+}
+
+function updatPostsInfo() {
+    getPosts().then((data) => {
+        console.log(data);
+        const postData = data.map((row) => {
+            let media = ""
+            if (row.media) {
+                switch (row.media.type) {
+                    case "image":
+                        media = `<div class="post-image"><img src="${row.media.url}" alt=""></div>`
+                        break;
+
+                    case "video":
+                        media = `<video controls><source src="${row.media.url}" type="video/mp4"></video>`
+                        break;
+                }
+            }
+
+            return `<div class="post">
+                <div class="post-author">
+                <span class="post-author-info">
+                    <img src="${row.author.avatar}" alt="Post author">
+                    <small>${row.author.firstname} ${row.author.lastname}</small>
+                </span>
+                <small>${row.createTime}</small>
+                </div>
+                ${media}        
+                <div class="post-title">
+                <h3>${row.text ? row.text : ""}</h3>
+                </div>
+                <div class="post-actions">
+                <button type="button" name="like" class="like-button">${row.likes}</button>
+                </div>
+            </div>`
+        }).join("")
+        $(".main-container").html(postData)
     });
 }
 
